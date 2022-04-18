2d604c9141f824668a4696ce137b33b51a6c4cf9
jotopozo <109-jotopozo@users.noreply.gitlab.espol.edu.ec>
2022-04-18 12:29:48 -0500

    V1 API de casa inteligente

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+BASE_URL = "/api"
+LIGHT = "/light"
+
+@app.route("/")
+def home():
+    '''
+    Página de inicio de api web
+    '''
+    return "<h1>CASA INTELIGENTE API!</h1>"
+
+@app.route(BASE_URL + LIGHT + "/on",methods=['POST'])
+def ligth_on():
+    '''
+    Encender led
+    '''
+    #DO IT STUFF
+    return 200
+
+@app.route(BASE_URL + LIGHT + "/off",methods=['POST'])
+def ligth_off():
+    '''
+    Apagar led
+    '''
+    #DO IT STUFF
+    return 200
+
